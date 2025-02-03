8e9881b575edfa3a7dc8e0708568b5ba5390f06c
Giovanna Diniz <143888056+giovannadiniz@users.noreply.github.com>
2025-02-03 15:57:27 -0300

    ComercioDTO

---
+++ src/main/java/com/placeti/avaliacao/dto/ComercioDTO.java
@@ -0,0 +1,25 @@
+//package com.placeti.avaliacao.dto;
+//
+//import com.placeti.avaliacao.model.Comercio;
+//import com.placeti.avaliacao.model.TipoComercio;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//
+//@Data
+//@NoArgsConstructor
+//public class ComercioDTO {
+//
+//    private Long id;
+//    private String nomeComercio;
+//    private String nomeResponsavel;
+//    private TipoComercio tipoComercio;
+//    private Long cidadeId;
+//
+//    public ComercioDTO(Comercio comercio) {
+//        this.id = comercio.getId();
+//        this.nomeComercio = comercio.getNomeComercio();
+//        this.nomeResponsavel = comercio.getNomeResponsavel();
+//        this.tipoComercio = comercio.getTipoComercio();
+//        this.cidadeId = comercio.getCidade().getId();
+//    }
+//}
