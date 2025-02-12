9a4c1d8b83b0798b263603cf5bdad1a3c5fa3bfb
Giovanna Diniz <143888056+giovannadiniz@users.noreply.github.com>
2025-02-11 22:32:42 -0300

    Update Comercio.java

--- src/main/java/com/placeti/avaliacao/model/Comercio.java
+++ src/main/java/com/placeti/avaliacao/model/Comercio.java
@@ -1,64 +1,31 @@
-//package com.placeti.avaliacao.model;
-//
-//
-//import jakarta.persistence.*;
-//import lombok.Data;
-//import lombok.NoArgsConstructor;
-//
-//@Data
-//@NoArgsConstructor
-//@Entity
-//@Table(name = "Comercio")
-//public class Comercio {
-//
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    @Column(name = "ID", nullable = false)
-//    private Long id;
-//
-//    @Column(name = "NOME_COMERCIO", length = 100, nullable = false)
-//    private String nomeComercio;
-//
-//    @Column(name = "NOME_RESPONSAVEL", length = 100, nullable = false)
-//    private String nomeResponsavel;
-//
-//    @Enumerated(EnumType.STRING)
-//    @Column(name = "TIPO_COMERCIO", nullable = false)
-//    private TipoComercio tipoComercio;
-//
-//    @ManyToOne
-//    @JoinColumn(name = "CIDADE_ID", nullable = false)
-//    private Cidade cidade;
-//}
-//package com.placeti.avaliacao.model;
-//
-//
-//import jakarta.persistence.*;
-//import lombok.Data;
-//import lombok.NoArgsConstructor;
-//
-//@Data
-//@NoArgsConstructor
-//@Entity
-//@Table(name = "Comercio")
-//public class Comercio {
-//
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    @Column(name = "ID", nullable = false)
-//    private Long id;
-//
-//    @Column(name = "NOME_COMERCIO", length = 100, nullable = false)
-//    private String nomeComercio;
-//
-//    @Column(name = "NOME_RESPONSAVEL", length = 100, nullable = false)
-//    private String nomeResponsavel;
-//
-//    @Enumerated(EnumType.STRING)
-//    @Column(name = "TIPO_COMERCIO", nullable = false)
-//    private TipoComercio tipoComercio;
-//
-//    @ManyToOne
-//    @JoinColumn(name = "CIDADE_ID", nullable = false)
-//    private Cidade cidade;
-//}
+package com.placeti.avaliacao.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "Comercio")
+public class Comercio {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID", nullable = false)
+    private Long id;
+
+    @Column(name = "NOME_COMERCIO", length = 100, nullable = false)
+    private String nomeComercio;
+
+    @Column(name = "NOME_RESPONSAVEL", length = 100, nullable = false)
+    private String nomeResponsavel;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "TIPO_COMERCIO", nullable = false)
+    private TipoComercio tipoComercio;
+
+    @ManyToOne
+    @JoinColumn(name = "CIDADE_ID", nullable = false)
+    private Cidade cidade;
+}
